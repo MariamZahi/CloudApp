e5bf255d4ff1d1b45422fe5c34426fdeb14f7d99
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-11-10 05:43:07 -0500

    Add project files.

---
+++ MauiApp16/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiApp16
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPredictClicked(object sender, EventArgs e)
+        {
+            string selectedDisease = diseasePicker.SelectedItem?.ToString();
+
+            if (string.IsNullOrEmpty(selectedDisease))
+            {
+                DisplayAlert("Error", "Please select a condition", "OK");
+                return;
+            }
+
+            string prediction = PredictWeather(selectedDisease);
+            predictionLabel.Text = $"Likely weather conditions for {selectedDisease}: {prediction}";
+        }
+
+        private string PredictWeather(string disease)
+        {
+            // Simple mapping for demonstration purposes
+            switch (disease.ToLower())
+            {
+                case "flu":
+                    return "Cold weather, winter months";
+                case "cough":
+                    return "Cold or dry weather";
+                case "fever":
+                    return "Hot weather, summer months";
+                case "nausea":
+                    return "Hot weather, summer months";
+                case "headache":
+                    return "Weather changes, low pressure";
+                case "bleeding":
+                    return "No specific weather correlation";
+                default:
+                    return "No prediction available";
+            }
+        }
+    }
+}
