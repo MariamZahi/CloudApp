cf0f1343a35f54c1e5a3264303e76fbe8f64a08f
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-11-30 23:49:38 -0500

    API xaml added

---
+++ MauiApp16/WeatherApiPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace MauiApp16
+{
+    public partial class WeatherApiPage : ContentPage
+    {
+        private MainViewModel _viewModel;
+
+        public WeatherApiPage()
+        {
+            InitializeComponent();
+            _viewModel = new MainViewModel();
+            BindingContext = _viewModel;
+        }
+
+        private async void OnGetWeatherClicked(object sender, EventArgs e)
+        {
+            await _viewModel.GetWeather();
+            weatherInfoLabel.Text = _viewModel.WeatherInfo;
+        }
+    }
+}
